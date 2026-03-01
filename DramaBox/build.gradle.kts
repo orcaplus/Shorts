cb22e40cd580d02239cf3942c5093bde04698b1a
orcaplus <158179215+orcaplus@users.noreply.github.com>
2026-03-01 08:38:47 +0700

    Create build.gradle.kts

---
+++ DramaBox/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    authors = listOf("orcaplus")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "AsianDrama",
+    )
+    isCrossPlatform = true
+    iconUrl = "https://raw.githubusercontent.com/orcaplus/png/refs/heads/main/orcaplus.png"
+}
